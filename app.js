4442e87c4856c3ecd00fcfef405c880492d4e768
tishvo <tishvo@gmail.com>
2021-05-07 11:26:42 -0400

    get req for answers is completed

--- app.js
+++ app.js
@@ -13,14 +13,14 @@ app.use(require("morgan")("dev"));
 
 app.get("/qa/questions", (req, res) => {
   var product_id = req.query['product_id'];
-  var query = `SELECT * FROM questions WHERE product_id=${product_id} AND reported=0;`
+  var query = `SELECT * FROM questions WHERE product_id=${product_id} AND reported=0 LIMIT 5;`;
 
   db.queryAsync(query) 
   .then(async function _(questions) {
     for (var i = 0; i < questions.length; i++) {
       const currentQ = questions[i];
       var q_id = currentQ['id'];
-      var a_query = `SELECT * FROM answers WHERE question_id=${q_id} AND reported=0;`
+      var a_query = `SELECT * FROM answers WHERE question_id=${q_id} AND reported=0;`;
       currentQ.answers = await db.queryAsync(a_query);
     }
     res.send({
@@ -30,6 +30,26 @@ app.get("/qa/questions", (req, res) => {
   })
   .catch(err => {
     res.send('error retrieving data');
+  })
+});
+
+app.get("/qa/questions/:question_id/answers", (req, res) => {
+  var question_id = req.params.question_id;
+  var query = `SELECT * FROM answers WHERE question_id=${question_id} AND reported=0 LIMIT 5;`;
+  db.queryAsync(query)
+  .then(async function _(answers) {
+    for (var i = 0; i < answers.length; i++) {
+      const currentA = answers[i];
+      var a_id = currentA['id'];
+      var photo_query = `SELECT * FROM answer_photos WHERE answer_id=${a_id};`;
+      currentA.photos = await db.queryAsync(photo_query);
+    }
+    res.send({
+      question: question_id,
+      page: 0,
+      count: 5,
+      results: answers
+    })
   })
 });
 
